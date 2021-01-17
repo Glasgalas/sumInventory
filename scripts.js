c3b339557175b21ed1662421b0fc55040e2f60f6
Alex Starichenko <68742649+Glasgalas@users.noreply.github.com>
2021-01-17 13:38:37 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function functionE() {
+  const x =
+    (parseInt(a.value) / parseInt(b.value)) *
+    parseInt(c.value).value.toFixed(2);
+  document.getElementById("info").innerHTML =
+    "Сумма недостачи на сотрудника: " + x;
+}
